d627b031881ac2a2db8fe3f06decff9012c32270
Daksh <70752231+daksh2703@users.noreply.github.com>
2022-10-26 00:15:20 +0530

    Add files via upload

---
+++ chat_page.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyADhBtTCLAXi_GqAn-ZJFW4qhijEaho3uM",
+    authDomain: "school-chatapp-cfbf6.firebaseapp.com",
+    databaseURL: "https://school-chatapp-cfbf6-default-rtdb.firebaseio.com",
+    projectId: "school-chatapp-cfbf6",
+    storageBucket: "school-chatapp-cfbf6.appspot.com",
+    messagingSenderId: "924366453261",
+    appId: "1:924366453261:web:b255e4c1348db40a997344"
+  };
+    
+    // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
